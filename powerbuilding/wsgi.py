174145e3e275106bbba7cefa36e3543d7056e7d9
Chetram Bassit <chetrambassit121@gmail.com>
2022-04-18 11:12:33 -0400

    removed defualt to prod added dev

--- powerbuilding/wsgi.py
+++ powerbuilding/wsgi.py
@@ -11,13 +11,13 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dotenv import load_dotenv
 
-from django.core.wsgi import get_wsgi_application
+
 
 
 
 load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'powerbuilding.settings')     # default
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'powerbuilding.settings')   # edited above line ... added .dev so it will default to our powerbuilding/settings/dev.py
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'powerbuilding.settings.dev')   # edited above line ... added .dev so it will default to our powerbuilding/settings/dev.py
 
 
 application = get_wsgi_application()
