3a6934633f014148e2d97269c8446ea68f1326fd
Chetram Bassit <chetrambassit121@gmail.com>
2022-04-18 12:33:41 -0400

    edited default added prod in init

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python manage.py migrate && gunicorn powerbuilding.wsgi --log-file - 
+web: gunicorn powerbuilding.wsgi --log-file - 
--- powerbuilding/wsgi.py
+++ powerbuilding/wsgi.py
@@ -16,8 +16,8 @@ from dotenv import load_dotenv
 
 
 load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'powerbuilding.settings')     # default
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'powerbuilding.settings.dev')   # edited above line ... added .dev so it will default to our powerbuilding/settings/dev.py
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'powerbuilding.settings')     # default
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'powerbuilding.settings.dev')   # edited above line ... added .dev so it will default to our powerbuilding/settings/dev.py
 
 
 application = get_wsgi_application()
