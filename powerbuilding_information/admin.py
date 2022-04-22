f2389d03ebcc6efae7e22f3370dcacd7a371f884
Chetram Bassit <chetrambassit121@gmail.com>
2022-04-21 20:24:53 -0400

    added survey model

--- powerbuilding_information/admin.py
+++ powerbuilding_information/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Survey
 
+admin.site.register(Survey)
 # Register your models here.
--- powerbuilding_information/models.py
+++ powerbuilding_information/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+# from django.contrib.auth.models import User     
+from members.models import User        
+from django.urls import reverse	
 
 # Create your models here.
+class Survey(models.Model):
+	question = models.CharField(null=True, blank=True, max_length=255)
+	extra_info = models.CharField(null=True, blank=True, max_length=1000)
+	likes = models.ManyToManyField(User, blank=True ,related_name='likes')
+	dislikes = models.ManyToManyField(User, blank=True ,related_name='dislikes')  
+
+	def get_absolute_url(self):
+		return reverse('surveys', args=[str(self.id)])
+	
+	def num_likes(self):
+		return self.likes.count()
+
+	def num_dislikes(self):
+		return self.dislikes.count()
