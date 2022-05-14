8a6c9311d5467da2e990fd7b110bdd36fec5d8fb
Chetram Bassit <chetrambassit121@gmail.com>
2022-05-14 19:28:42 -0400

    tweakeing email settings for travis conecction

--- powerbuilding/settings/base.py
+++ powerbuilding/settings/base.py
@@ -219,16 +219,24 @@ LOGOUT_REDIRECT_URL = 'home'                                                    
 
 # added all email information which connects to the information in .env file .. we do not want our sensative information displayed here rather its displayed in hidden .env file
 # EMAIL_BACKEND = config('EMAIL_BACKEND') 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' 
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_BACKEND=config('EMAIL_BACKEND')
+EMAIL_USE_TLS=config('EMAIL_USE_TLS')
+EMAIL_HOST=config('EMAIL_HOST')
+EMAIL_HOST_USER=config('EMAIL_HOST_USER')
+DEFAULT_FROM_EMAIL=config('DEFAULT_FROM_EMAIL')
+EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT=config('EMAIL_PORT') 
 
 
-EMAIL_BACKEND = config('EMAIL_BACKEND')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = config('EMAIL_PORT')
+# EMAIL_BACKEND = config('EMAIL_BACKEND')
+# EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+# EMAIL_HOST = config('EMAIL_HOST')
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# EMAIL_PORT = config('EMAIL_PORT')
 
 # EMAIL_HOST = os.getenv('EMAIL_HOST')
 # EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
